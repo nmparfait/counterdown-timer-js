c8fe931884f2c3aafe6023180da3a9425a5ba5eb
nmparfait <mbamu.developer@gmail.com>
2021-04-05 15:39:33 -0400

    set the missing hours

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ const weekdays = [
 
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
-const items = document.querySelectorAll('.dealine-format h4');
+const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2021, 3, 5, 11, 45, 0);
+let futureDate = new Date(2021, 3, 7, 11, 45, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -36,3 +36,44 @@ month = months[month];
 const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 giveaway.textContent = `giveaway end on ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60 min
+  // 1d = 24h
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  //calculate all the values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  //set the format
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  //set values array
+  const values = [days, hours, minutes, seconds];
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+}
+
+getRemainingTime();
