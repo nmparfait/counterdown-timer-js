2ca058d27d2ef0d82c0ab7a2b50ad7e1bbdcaae1
nmparfait <mbamu.developer@gmail.com>
2021-04-05 14:54:16 -0400

    working with date

---
+++ app.js
@@ -0,0 +1,38 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.dealine-format h4');
+
+let futureDate = new Date(2021, 3, 5, 11, 45, 0);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+giveaway.textContent = `giveaway end on ${date} ${month} ${year} ${hours}:${minutes}am`;
